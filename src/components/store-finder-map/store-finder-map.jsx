ff4cd94a2c404c33baf2b3cf93f31dbe0e94c815
motorolla29 <eutyou@gmail.com>
2024-04-23 17:08:57 +0300

    init clusters

--- src/components/store-finder-map/store-finder-map.jsx
+++ src/components/store-finder-map/store-finder-map.jsx
@@ -1,4 +1,5 @@
-import Map, {
+import { useRef, useState } from 'react';
+import {
   NavigationControl,
   GeolocateControl,
   FullscreenControl,
@@ -6,14 +7,41 @@ import Map, {
   Marker,
   Popup,
 } from 'react-map-gl/maplibre';
+import { Map, Source, Layer } from 'react-map-gl/maplibre';
 
 import MapPin from '../map-pin/map-pin';
+import {
+  clusterLayer,
+  clusterCountLayer,
+  unclusteredPointLayer,
+} from '../../map-layers';
+import stores from '../../mocks/stores.geojson';
 
 import './store-finder-map.sass';
-import { useState } from 'react';
 
 const StoreFinderMap = () => {
+  const mapRef = useRef(null);
   const [popupInfo, setPopupInfo] = useState(null);
+
+  const onMapClick = (event) => {
+    const feature = event.features[0];
+    const clusterId = feature?.properties?.cluster_id;
+
+    const mapboxSource = mapRef.current.getSource('stores');
+
+    mapboxSource.getClusterExpansionZoom(clusterId, (err, zoom) => {
+      if (err) {
+        return;
+      }
+
+      mapRef.current.easeTo({
+        center: feature.geometry.coordinates,
+        zoom,
+        duration: 500,
+      });
+    });
+  };
+
   return (
     <Map
       initialViewState={{
@@ -25,8 +53,36 @@ const StoreFinderMap = () => {
         'https://api.maptiler.com/maps/streets/style.json?key=JiORwzpLecOFb1wih0mU'
       }
       attributionControl={false}
+      interactiveLayerIds={[clusterLayer.id]}
+      onClick={onMapClick}
+      ref={mapRef}
     >
-      <Marker
+      <Source
+        id="stores"
+        type="geojson"
+        data={stores}
+        cluster={true}
+        clusterMaxZoom={14}
+        clusterRadius={50}
+      >
+        <Layer {...clusterLayer} />
+        <Layer {...clusterCountLayer} />
+        <Layer
+          id="unclustered-point"
+          type="circle"
+          source="stores"
+          filter={['!', ['has', 'point_count']]}
+          paint={{
+            'circle-color': '#11b4da',
+            'circle-radius': 4,
+            'circle-stroke-width': 1,
+            'circle-stroke-color': '#fff',
+          }}
+        >
+          <MapPin />
+        </Layer>
+      </Source>
+      {/* <Marker
         longitude={37.38720655414402}
         latitude={55.811490987502005}
         anchor="top"
@@ -40,7 +96,7 @@ const StoreFinderMap = () => {
         }}
       >
         <MapPin />
-      </Marker>
+      </Marker> */}
 
       {popupInfo && (
         <Popup
