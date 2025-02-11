447c04316977aedd5f7ebd6d254fa0a03bb2e25d
motorolla29 <eutyou@gmail.com>
2025-02-11 19:34:39 +0300

    create order with auth host

--- src/api/orderAPI.js
+++ src/api/orderAPI.js
@@ -1,6 +1,6 @@
-import { $host } from './index';
+import { $authHost } from './index';
 
 export const createOrder = async (orderData) => {
-  const { data } = await $host.post('api/orders/create', orderData);
+  const { data } = await $authHost.post('api/orders/create', orderData);
   return data;
 };
