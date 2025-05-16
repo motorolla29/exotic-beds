5eb8879e7fd865195831d21ae72f19c62332e6c1
motorolla29 <eutyou@gmail.com>
2025-05-16 11:26:14 +0300

    catalog filters skeleton refinement

---
+++ src/components/catalog-filters/catalog-filters-skeleton.jsx
@@ -0,0 +1,45 @@
+import './catalog-filters-skeleton.sass';
+
+const CatalogFiltersSkeleton = () => {
+  return (
+    <div className="catalog-filters-skeleton">
+      <div className="cf-skel-header catalog-filters-skeleton-shine" />
+      <div className="cf-skel-filter_1">
+        <div className="cf-skel-filter_1_title catalog-filters-skeleton-shine" />
+        <div className="cf-skel-filter_1_body">
+          <div className="cf-skel-filter_1_body_1">
+            <div className="cf-skel-filter_1_body_1_1 catalog-filters-skeleton-shine" />
+            <div className="cf-skel-filter_1_body_1_2 catalog-filters-skeleton-shine" />
+          </div>
+          <div className="cf-skel-filter_1_body_2 catalog-filters-skeleton-shine" />
+        </div>
+      </div>
+      <div className="cf-skel-filter_2">
+        <div className="cf-skel-filter_2_title catalog-filters-skeleton-shine" />
+        <div className="cf-skel-filter_2_body">
+          <div className="cf-skel-filter_2_body_1 catalog-filters-skeleton-shine" />
+          <div className="cf-skel-filter_2_body_2 catalog-filters-skeleton-shine" />
+          <div className="cf-skel-filter_2_body_3 catalog-filters-skeleton-shine" />
+          <div className="cf-skel-filter_2_body_4 catalog-filters-skeleton-shine" />
+          <div className="cf-skel-filter_2_body_5 catalog-filters-skeleton-shine" />
+        </div>
+      </div>
+      <div className="cf-skel-filter_3">
+        <div className="cf-skel-filter_3_1">
+          <div className="cf-skel-filter_3_1_1 catalog-filters-skeleton-shine" />
+          <div className="cf-skel-filter_3_1_2 catalog-filters-skeleton-shine" />
+        </div>
+        <div className="cf-skel-filter_3_2">
+          <div className="cf-skel-filter_3_2_1 catalog-filters-skeleton-shine" />
+          <div className="cf-skel-filter_3_2_2 catalog-filters-skeleton-shine" />
+        </div>
+        <div className="cf-skel-filter_3_3">
+          <div className="cf-skel-filter_3_3_1 catalog-filters-skeleton-shine" />
+          <div className="cf-skel-filter_3_3_2 catalog-filters-skeleton-shine" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CatalogFiltersSkeleton;
--- src/components/catalog-filters/catalog-filters.jsx
+++ src/components/catalog-filters/catalog-filters.jsx
@@ -7,6 +7,7 @@ import PriceFilter from '../filter-components/price-filter/price-filter';
 import RatingFilter from '../filter-components/rating-filter/rating-filter';
 import SeriesFilter from '../filter-components/series-filter/series-filter';
 import FilterSwitchers from '../filter-components/filter-switchers/filter-switchers';
+import CatalogFiltersSkeleton from './catalog-filters-skeleton';
 
 import './catalog-filters.sass';
 
@@ -44,44 +45,7 @@ const CatalogFilters = ({
   };
 
   return loading ? (
-    <div className="catalog-filters-skeleton">
-      <div className="cf-skel-header catalog-filters-skeleton-shine" />
-      <div className="cf-skel-filter_1">
-        <div className="cf-skel-filter_1_title catalog-filters-skeleton-shine" />
-        <div className="cf-skel-filter_1_body">
-          <div className="cf-skel-filter_1_body_1">
-            <div className="cf-skel-filter_1_body_1_1 catalog-filters-skeleton-shine" />
-            <div className="cf-skel-filter_1_body_1_2 catalog-filters-skeleton-shine" />
-          </div>
-          <div className="cf-skel-filter_1_body_2 catalog-filters-skeleton-shine" />
-        </div>
-      </div>
-      <div className="cf-skel-filter_2">
-        <div className="cf-skel-filter_2_title catalog-filters-skeleton-shine" />
-        <div className="cf-skel-filter_2_body">
-          <div className="cf-skel-filter_2_body_1 catalog-filters-skeleton-shine" />
-          <div className="cf-skel-filter_2_body_2 catalog-filters-skeleton-shine" />
-          <div className="cf-skel-filter_2_body_3 catalog-filters-skeleton-shine" />
-          <div className="cf-skel-filter_2_body_4 catalog-filters-skeleton-shine" />
-          <div className="cf-skel-filter_2_body_5 catalog-filters-skeleton-shine" />
-        </div>
-      </div>
-      <div className="cf-skel-filter_3">
-        <div className="cf-skel-filter_3_title catalog-filters-skeleton-shine" />
-        <div className="cf-skel-filter_3_1">
-          <div className="cf-skel-filter_3_1_1 catalog-filters-skeleton-shine" />
-          <div className="cf-skel-filter_3_1_2 catalog-filters-skeleton-shine" />
-        </div>
-        <div className="cf-skel-filter_3_2">
-          <div className="cf-skel-filter_3_2_1 catalog-filters-skeleton-shine" />
-          <div className="cf-skel-filter_3_2_2 catalog-filters-skeleton-shine" />
-        </div>
-        <div className="cf-skel-filter_3_3">
-          <div className="cf-skel-filter_3_3_1 catalog-filters-skeleton-shine" />
-          <div className="cf-skel-filter_3_3_2 catalog-filters-skeleton-shine" />
-        </div>
-      </div>
-    </div>
+    <CatalogFiltersSkeleton />
   ) : (
     <StickyBox className="catalog-filters" offsetTop={20} offsetBottom={20}>
       <div className="catalog-filters_header">
